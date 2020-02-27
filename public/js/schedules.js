c4e5af418cbf849fbd693f3c3a5a199ad9305830
aramiro <aramiro@ucsd.edu>
2020-02-27 11:26:46 -0800

    nudge added

--- public/js/schedules.js
+++ public/js/schedules.js
@@ -1,48 +1,98 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var calendarEl = document.getElementById('calendar');
-  
-	var calendar = new FullCalendar.Calendar(calendarEl, {
-	  plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap', 'googleCalendar' ],
-	  googleCalendarApiKey:'AIzaSyD_OFNIKMzCwC7ZotObECp4sUNZ7hH-iv0',
-	  defaultView: 'dayGridMonth',
-	  defaultDate: '2020-02-07',
-	  header: {
-		left: 'prev,next today',
-		center: 'title',
-		right: 'dayGridMonth,timeGridWeek,timeGridDay'
-	  },
-	  events: 
-			{
-				googleCalendarId: 'ucsd.edu_87nar99a1vmecvl0g75e4ubs4s@group.calendar.google.com'
-			}
-	  ,
+document.addEventListener("DOMContentLoaded", function() {
+  var calendarEl = document.getElementById("calendar");
+  var calendarE2 = document.getElementById("calendar2");
 
-	  dateClick: function(info) {
-		//alert('Clicked on: ' + info.dateStr);
-		//alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-		//alert('Current view: ' + info.view.type);
-		// change the day's background color just for fun
-		info.dayEl.style.backgroundColor = 'red';
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [
+      "interaction",
+      "dayGrid",
+      "timeGrid",
+      "list",
+      "bootstrap",
+      "googleCalendar"
+    ],
+    editable: true,
+    googleCalendarApiKey: "AIzaSyD_OFNIKMzCwC7ZotObECp4sUNZ7hH-iv0",
+    defaultView: "dayGridMonth",
+    defaultDate: "2020-02-07",
+    header: {
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,timeGridWeek,timeGridDay"
+    },
+    events: {
+      googleCalendarId:
+        "ucsd.edu_87nar99a1vmecvl0g75e4ubs4s@group.calendar.google.com"
+    },
+    dateClick: function(info) {
+      //alert('Clicked on: ' + info.dateStr);
+      //alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+      //alert('Current view: ' + info.view.type);
+      // change the day's background color just for fun
+      info.dayEl.style.backgroundColor = "red";
 
-		$("#popup").css("display", "block");
+      $("#popup").css("display", "block");
+    }
 
-	  } 
-	  /*,
-
-	  eventClick: function(info) {
-		  $("nudge").css("display", "");
-	  }
-*/
-	});
-  
-	calendar.render();
 
   });
 
-  $('#toggle > div').click(function() {
-    var ix = $(this).index();
-    
-    $('#individualCalendar').toggle( ix === 0 );
-    $('#groupCalendar').toggle( ix === 1 );
-});
+  var gcalendar = new FullCalendar.Calendar(calendarE2, {
+    plugins: [
+      "interaction",
+      "dayGrid",
+      "timeGrid",
+      "list",
+      "bootstrap",
+      "googleCalendar"
+    ],
+    editable: true,
+    googleCalendarApiKey: "AIzaSyD_OFNIKMzCwC7ZotObECp4sUNZ7hH-iv0",
+    defaultView: "dayGridMonth",
+    defaultDate: "2020-02-07",
+    header: {
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,timeGridWeek,timeGridDay"
+    },
+    events: {
+      googleCalendarId:
+        "ucsd.edu_dfuugi6lj86saaqjrnkfgdfd6s@group.calendar.google.com"
+    },
+    dateClick: function(info) {
+      //alert('Clicked on: ' + info.dateStr);
+      //alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+      //alert('Current view: ' + info.view.type);
+      // change the day's background color just for fun
+      info.dayEl.style.backgroundColor = "red";
 
+      $("#popup").css("display", "block");
+    },
+    eventClick: function(info) {
+		info.jsEvent.preventDefault(); // don't let the browser navigate
+  
+		var modal = document.getElementById("nudgeModal");
+		var span = document.getElementsByClassName("close")[0];
+		var cancel = document.getElementById("close");
+		modal.style.display = "block";
+  
+		span.onclick = function() {
+		  modal.style.display = "none";
+		};
+		cancel.onclick = function(){
+			modal.style.display = "none";
+		}
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		  if (event.target == modal) {
+			modal.style.display = "none";
+		  }
+		};
+		// change the border color just for fun
+		info.el.style.borderColor = "red";
+	  }
+  });
+
+  calendar.render();
+  gcalendar.render();
+});
